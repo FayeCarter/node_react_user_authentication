945b764177b0eab83276d8ed239ed8130de6fdc8
FayeCarter <fayecarter333@gmail.com>
2020-05-23 15:47:37 +0100

    cleanup routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,12 +7,7 @@ const jwtGenerator = require("../utils/jwtGenerator");
 
 router.post("/register", async (req, res) => {
   try {
-    //get name, email and password fro req.body
-
     const { name, email ,password } = req.body
-
-    //check if user exists -> throw error
-
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
       email
     ]);
@@ -21,22 +16,14 @@ router.post("/register", async (req, res) => {
       return res.status(401).send("User already Exists!")
     }
 
-    //BCRYPT user password
-
     const saltRound = 10;
     const salt = await bcrypt.genSalt(saltRound);
-
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-    //enter new user inside database
-
     const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES($1, $2, $3) RETURNING *", [name, email, bcryptPassword])
-
-    
-    //generate jwt token
     const token = jwtGenerator(newUser.rows[0].id)
-     
     res.json({ token })
+    
   } catch (error) {
     console.error(error.message)
     res.status(500).send("Server Error");
@@ -46,13 +33,7 @@ router.post("/register", async (req, res) => {
 //login
 router.post("/login", async (req, res) => {
   try {
-
-    // destructure res.body
-
     const {email, password} = req.body
-
-    // check if user exists -> throw error
-
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
       email
     ]);
@@ -61,18 +42,12 @@ router.post("/login", async (req, res) => {
       return res.status(401).json("Password or Email is incorrect")
     }
 
-    // incoming == database password
-
     const validPassword = await bcrypt.compare(password, user.rows[0].user_password)
-
     if(!validPassword) {
       return res.status(401).json("Password or Email is incorrect")
     }
 
-    //give token
-
     const token = jwtGenerator(user.rows[0].id)
-
     res.json({ token })
 
   } catch (error) {
